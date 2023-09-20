55cee96b96b0de02d111c4485d8a03af9c91405b
KRATOS <samak519@student.liu.se>
2023-09-20 13:38:35 +0200

    add favicon, format with css!

--- cgi/FetchObjectIds.sh
+++ cgi/FetchObjectIds.sh
@@ -15,14 +15,10 @@ jsonUrl="$baseUrl/objects.json?l=sv_SE&search_text=${course}&types=219&fe=132.0&
 # Use curl to fetch the JSON data containing object ids for coursename
 objectIds=$(curl -s "$jsonUrl" | jq -r '.records[].identVirtual')
 
-scheduleUrl="https://cloud.timeedit.net/liu/web/schema/ri.html?h=t&sid=3&p=20230802%2C20231231&objects=$(IFS=,; echo "${objectIds[*]}")&ox=0&types=0"
-
-# Use curl to fetch the HTML data for the scheduleUrl
-html=$(curl -s "$scheduleUrl")
-
-# Count the occurrences of td elements with class ffheader
-count=$(echo "$html" | grep -o '<td class="ffheader">' | wc -l)
+semesterUrl="https://cloud.timeedit.net/liu/web/schema/ri.html?h=t&sid=3&p=20230801%2C20231231&objects=$(IFS=,; echo "${objectIds[*]}")&ox=0&types=0"
+futureUrl="https://cloud.timeedit.net/liu/web/schema/ri.html?h=t&sid=3&p=0.d%2C20231231.x&objects=$(IFS=,; echo "${objectIds[*]}")&ox=0&types=0"
 
 echo "Content-type: text/html"
 echo
-echo "<a href=$scheduleUrl>Link to schedule</a>"
+echo "<div id="semesterUrlDiv" style="display:none">$semesterUrl</div>"
+echo "<div id="futureUrlDiv" style="display:none">$futureUrl</div>"
